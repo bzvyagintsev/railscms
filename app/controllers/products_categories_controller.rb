cd92bd70df409f1d4cacfbd7617cfb4e6def0d32
bzvyagintsev <b.zvyagintsev@gmail.com>
2016-04-12 23:19:58 +0300

    update

--- app/controllers/products_categories_controller.rb
+++ app/controllers/products_categories_controller.rb
@@ -7,6 +7,7 @@ class ProductsCategoriesController < ApplicationController
 		end
 
 		def show
+    		@products = @products_category.products.active.order('title ASC')
 		end
 
 
@@ -16,7 +17,7 @@ class ProductsCategoriesController < ApplicationController
 	# Поиск страницы по friendly_id и редирект с id на friendly_id
 	  def set_products_category
 	    # @products_category = ProductsCategories.find(params[:id])
-	    @products_category = ProductsCategories.friendly.find(params[:id])
+	    @products_category = ProductsCategory.friendly.find(params[:id])
 	    redirect_to action: :show, id: @products_category.friendly_id, status: 301 unless @products_category.friendly_id == params[:id] 
 	  end
 
